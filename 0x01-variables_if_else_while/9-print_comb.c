a62692dcba91f9efd2d10e14bb27762bbce76d7b
Ntshuxeko <ntshuxekomalmela027@gmail.com>
2023-07-14 16:12:25 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++) {
+	for (i = 0; i <= 9; i++) 
+	{
 		putchar('0' + i);
-	       	if (i != 9) {
+	       	if (i != 9) 
+		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
+	putchar('\n');
 
 	return (0);
 }
