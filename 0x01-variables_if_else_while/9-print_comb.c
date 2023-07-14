b97dabe4ffd7a42b6d3d12f9176e1be5032c7ed3
Ntshuxeko <ntshuxekomalmela027@gmail.com>
2023-07-14 16:14:55 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,16 @@
  */
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i <= 9; i++) 
+	for (num = 0; num <= 9; num++)
 	{
-		putchar('0' + i);
-	       	if (i != 9) 
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');
