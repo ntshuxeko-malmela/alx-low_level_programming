707e19b500c0ff9794954228b3973801770638bf
Ntshuxeko <ntshuxekomalmela027@gmail.com>
2023-07-18 13:28:19 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,15 +14,15 @@ void times_table(void)
 			product = row * column;
 
 			if (column != 0)
-			{
-				_putchar(',');
+ 			{
+  				_putchar(',');
 				_putchar(' ');
 			}
 
 			if (product >= 10)
 			{
 				_putchar((product / 10) + '0');
- 				_putchar((product % 10) + '0');
+				_putchar((product % 10) + '0');
 			}
 			else
 			{
@@ -32,6 +32,5 @@ void times_table(void)
 		}
 
 		_putchar('\n');
-	}
+		}
 }
-
