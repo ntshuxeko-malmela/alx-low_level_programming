9b7ba521606d4e0c4b0296c4b1a1a2caa8aa968e
Ntshuxeko <ntshuxekomalmela027@gmail.com>
2023-07-18 12:36:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+ 		{
+			_putchar(j);
+		}
+	}
+
+	_putchar('\n');
+}
